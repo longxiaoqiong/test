3aa9b214f5d3a8cf33c134658334e106eeb90643
xiaoqiong.long <xiaoqiong.long@SZASS1332.ucloudlink.com>
2018-04-25 17:10:06 +0800

    update

--- springcloud_eureka_server/src/main/java/com/springcloud/main/eurekaApplication.java
+++ springcloud_eureka_server/src/main/java/com/springcloud/main/eurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class eurekaApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		new SpringApplicationBuilder(eurekaApplication.class).web(true).run(args);
 	}
 }
