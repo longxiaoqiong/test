c9199e2a72de3ac4a92aa72723e0cc5cd87bbf7e
xiaoqiong.long <xiaoqiong.long@SZASS1332.ucloudlink.com>
2018-12-18 17:01:23 +0800

    111

--- springcloud_service/src/main/java/com/springcloud/ServiceApplication.java
+++ springcloud_service/src/main/java/com/springcloud/ServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class serviceApplication {
+public class ServiceApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(serviceApplication.class).web(true).run(args);
+		new SpringApplicationBuilder(ServiceApplication.class).web(true).run(args);
 	}
 }
